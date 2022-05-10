25df632d051ce0e345a7b1dd5c7379203134192c
JoseeCarlos <montanoj47@gmail.com>
2022-05-10 18:29:18 -0400

    desarollo del header, creacion de rutas y icons

--- website/app/sli_page.py
+++ website/app/sli_page.py
@@ -7,3 +7,15 @@ bp = Blueprint('sli_page', __name__, url_prefix='/')
 @bp.route('/', methods=['GET'])
 def index():
     return render_template('homepage/index.html')
+
+@bp.route('/features', methods=['GET'])
+def features():
+    return render_template('features/index.html')
+
+@bp.route('/news', methods=['GET'])
+def news():
+    return render_template('news/index.html')
+
+@bp.route('/test', methods=['GET'])
+def test():
+    return render_template('test/index.html')
