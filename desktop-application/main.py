3975e6125bb2bf17436a7dcc495840659aaa5b43
MirkoMarca <mmm2019275@est.univalle.edu>
2022-05-24 03:29:30 -0400

    Desarrollo de muestra y seleccion de camaras disponibles

--- desktop-application/main.py
+++ desktop-application/main.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 import tkinter.ttk as ttk
+import cv2
+from tkinter import messagebox
 class Aplication:
     def __init__(self):
         self.main = tk.Tk()
@@ -12,19 +14,39 @@ class Aplication:
         self.useInstructionsLabel.place(x=10,y=120,width=180)
         self.useInstructionsLabel=ttk.Label(self.main,text="Instrucciones en Aplicación:\n   1. Entrar aconfiguraciones de\n       entrada y salida.\n   2.Selecciona como web cam:\n      SLI Virtual Camera.\n   3. Selecciona como microfono:\n      SLI Virtual Mic.\n   4. Selecciona como Altavoz:\n      SLI Virtual Speaker")
         self.useInstructionsLabel.place(x=10,y=170,width=180)
+        #CameraList
+        capture=cv2.VideoCapture(0,cv2.CAP_DSHOW)
+        def returnCameraIndexes():
+            index = 0
+            arr = []
+            i = 10
+            while i > 0:
+                cap = cv2.VideoCapture(index,cv2.CAP_DSHOW)
+                if cap.read()[0]:
+                    arr.append('CAM'+str(index))
+                    cap.release()
+                index += 1
+                i -= 1
+            return arr
+        #cambio de seleccion combo camara
+        def selection_changed(event):
+            selection = self.cameraCombo.current()
+            #capture=cv2.VideoCapture(selection,cv2.CAP_DSHOW)
         #Componentes de frameCameraConfig
         self.cameraInputImg=tk.PhotoImage(file="CamView.gif")
         self.cameraInputImgLabel=tk.Label(self.main,image=self.cameraInputImg)
         self.cameraInputImgLabel.place(x=190,y=10,width=520,height=330)
         self.cameraLabel=ttk.Label(self.main,text="Selecciona Webcam")
         self.cameraLabel.place(x=400,y=340)
-        self.cameraCombo=ttk.Combobox(self.main,state="readonly",values=["Integrated Webcam","OBS Virtual Cam"])
-        self.cameraCombo.set("Integrated Webcam")
+        self.cameraCombo=ttk.Combobox(self.main,state="readonly",values=returnCameraIndexes())
+        self.cameraCombo.current(0)
+        self.cameraCombo.bind("<<ComboboxSelected>>", selection_changed)
         self.cameraCombo.place(x=190,y=360,width=520)
+
         self.languageLabel=ttk.Label(self.main,text="Selecciona Idioma")
         self.languageLabel.place(x=400,y=385)
         self.languageCombo=ttk.Combobox(self.main,state="readonly",values=["Español","English"])
-        self.languageCombo.set("Español")
+        self.languageCombo.current(0)
         self.languageCombo.place(x=190,y=405,width=520)
         self.startButton=ttk.Button(self.main,text="Iniciar")
         self.startButton.place(x=375,y=435,width=150)
