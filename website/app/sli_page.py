d46573dfa567fd2a9c3669750ff8ba8bc181bd80
Jose Carlos Colque Montaño <montanoj47@gmail.com>
2022-08-15 08:23:22 -0400

    uso de la camara

--- website/app/sli_page.py
+++ website/app/sli_page.py
@@ -1,8 +1,21 @@
 from flask import (
-    Blueprint, render_template
+    Blueprint, render_template, Response, request, redirect, url_for, flash,
 )
-
+import cv2
 bp = Blueprint('sli_page', __name__, url_prefix='/')
+
+camera=cv2.VideoCapture(0)
+
+def generate_frames():
+    while True:
+        success,frame=camera.read()
+        if not success:
+            break
+        else:
+            ret,buffer=cv2.imencode('.jpg',frame)
+            frame=buffer.tobytes()
+        yield(b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @bp.route('/', methods=['GET'])
 def index():
@@ -23,3 +36,11 @@ def test():
 @bp.route('/suport', methods=['GET'])
 def suport():
     return render_template('suport/index.html')
+
+@bp.route('/video_feed', methods=['GET'])
+def video_feed():
+    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
+    
+
+
+    
